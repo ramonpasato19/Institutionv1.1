241c9554a3d1e602f3a7d63945b7e180f9956dd3
larp <larp@LuisRamon>
2020-06-22 14:40:59 -0500

    add models

--- Institutionv1.1/src/main/java/inst/edu/model/Student.java
+++ Institutionv1.1/src/main/java/inst/edu/model/Student.java
@@ -1,5 +1,63 @@
 package inst.edu.model;
 
-public class Student {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+
+/**
+ * @author Luis Ramon
+ * @version 1.0.0 La entidad sirve para crear una tabla estudiante
+ */
+@Entity
+@Table(name = "student")
+public class Student implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "student_id")
+	private int studentId;
+
+    @Email
+    @Column(name = "email_institution")
+	private String emailInstitution;
+    
+	@Column(name = "code_student")
+	private String codeStudent;
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getEmailInstitution() {
+		return emailInstitution;
+	}
+
+	public void setEmailInstitution(String emailInstitution) {
+		this.emailInstitution = emailInstitution;
+	}
+
+	public String getCodeStudent() {
+		return codeStudent;
+	}
+
+	public void setCodeStudent(String codeStudent) {
+		this.codeStudent = codeStudent;
+	}
+	
+    
 
 }
--- Institutionv1.1/src/main/java/inst/edu/model/Teacher.java
+++ Institutionv1.1/src/main/java/inst/edu/model/Teacher.java
@@ -1,5 +1,61 @@
 package inst.edu.model;
 
-public class Teacher {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Email;
+
+/**
+ * @author Luis Ramon
+ * @version 1.0.0 La entidad sirve para crear una tabla docente
+ */
+@Entity
+@Table(name = "teacher")
+public class Teacher implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "teacher_id")
+	private int teacherId;
+
+    @Email
+    @Column(name = "email_institution")
+	private String emailInstitution;
+    
+	@Column(name = "code_teacher")
+	private String codeTeacher;
+
+	public int getTeacherId() {
+		return teacherId;
+	}
+
+	public void setTeacherId(int teacherId) {
+		this.teacherId = teacherId;
+	}
+
+	public String getEmailInstitution() {
+		return emailInstitution;
+	}
+
+	public void setEmailInstitution(String emailInstitution) {
+		this.emailInstitution = emailInstitution;
+	}
+
+	public String getCodeTeacher() {
+		return codeTeacher;
+	}
+
+	public void setCodeTeacher(String codeTeacher) {
+		this.codeTeacher = codeTeacher;
+	}
+	
+	
 
 }
