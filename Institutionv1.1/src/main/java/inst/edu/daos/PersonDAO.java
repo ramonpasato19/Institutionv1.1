e5b7b9968192f732258d8dc32a6791bcdc64a501
Hernan Morocho <herfeli97@gmail.com>
2020-07-15 20:56:42 -0500

    Create PersonDAO.java
    Creacion de los DAOS

---
+++ Institutionv1.1/src/main/java/inst/edu/daos/PersonDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inst.edu.daos;
+
+import inst.edu.model.Person;
+
+/**
+ *
+ * @author Hernan Morocho
+ */
+public class PersonDAO {
+    
+    public void savePerson(Person p){
+        
+        
+    }
+}
